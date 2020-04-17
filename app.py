7db9d097b187fa3af16d0904801aa957a1cc8180
luoxuqi <1576123082@qq.com>
2020-04-17 11:54:38 +0800

    updated

--- app.py
+++ app.py
@@ -12,10 +12,21 @@ def broadcast_room():
     message = request.get_json(force=True)
     if args_verification(message):
         print(message)
-        socketio.emit('room_message', {'message': message}, room=message['chatroom_id'])
+        socketio.emit('room_message', {'message': message}, room=str(message['chatroom_id']), broadcast=True)
         return statusVo(message="Broadcast successfully.", status="OK")
     else:
         return statusVo(message="Broadcast failed.", status="ERROR")
+
+
+@app.route("/send_file", methods=["GET", "POST"])
+def send_file():
+    message = request.get_json(force=True)
+    if args_verification(message):
+        print(message)
+        socketio.emit('file_message', {'file': message}, room=str(message['chatroom_id']), broadcast=True)
+        return statusVo(message="Send file successfully.", status="OK")
+    else:
+        return statusVo(message="Send file failed.", status="ERROR")
 
 
 @socketio.on('my event')
@@ -25,16 +36,19 @@ def my_event_handler(data):
 
 @socketio.on('join')
 def on_join(data):
+    print(data)
     room = data['chatroom_id']
     join_room(room)
-    emit('status', 'Entered the room ' + room)
+    print("someone enter: ", room)
+    emit('status', {'status': 'in', 'room': room})
 
 
 @socketio.on('leave')
 def on_leave(data):
     room = data['chatroom_id']
     leave_room(room)
-    emit('status', 'Left the room ' + room)
+    print("someone left: ", room)
+    emit('status', {'status': 'out', 'room': room})
 
 
 @socketio.on('text')
